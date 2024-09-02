972a5971aca140e04844947f21e72cceea1afa1b
Deepansh Saxena <121677704+saxenadeep@users.noreply.github.com>
2024-09-02 21:06:06 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is a Python?",
+        answer: "D. Programming Language",
+        options: [
+            "A. Word",
+            "B. language",
+            "C. Famous place",
+            "D. Programming Language",
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which type of Language is a Python?",
+        answer: "B. Interpreted",
+        options: [
+            "A. Compiled",
+            "B. Interpreted",
+            "C. Declaration",
+            "D. None",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who developed Python Programming Language?",
+        answer: "C. Guido van Rossum",
+        options: [
+            "A. Wick van Rossum",
+            "B. Rasmus Lerdorf",
+            "C. Guido van Rossum",
+            "D. Niene Stom",
+            
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which language is most trending acc to tiobe index?",
+        answer: "A. Python",
+        options: [
+            "A. Python",
+            "B. C",
+            "C. C++",
+            "D. Java",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which one of the following is a framework of python",
+        answer: "D. Django",
+        options: [
+            "A. Javascript",
+            "B. Mongodb",
+            "C. Nodejs",
+            "D. Django",
+        ]
+    }
+
+];
